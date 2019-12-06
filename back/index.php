cc8ef1784a76d03b088f16bc06c20a0b6343b12b
Swannn <swann.juillard@gmail.com>
2019-12-06 02:36:46 +0000

    Fin partie BACK TP05

--- back/index.php
+++ back/index.php
@@ -1,16 +1,72 @@
 <?php
 require 'vendor/autoload.php';
 
-$client = array();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Firebase\JWT\JWT;
+
+const CLE = "cledeswann";
 
 $app = new \Slim\App;
 
+$jwt = new \Slim\Middleware\JwtAuthentication([
+    "path" => "/api",
+    "secure" => false,
+    "passthrough" => ["/api/users/login","/api/users/register"],
+    "secret" => CLE,
+    "attribute" => "decoded_token_data",
+    "algorithm" => ["HS256"],
+    "error" => function ($response, $arguments) {
+        $data = array('ERREUR' => 'ERREUR', 'ERREUR' => 'AUTO');
+        return $response->withHeader("Content-Type", "application/json")->getBody()->write(json_encode($data));
+    }
+]);
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+$app->add($jwt);
+
 $app->get('/produits', 'getProduits');
 
-$app->get('/client/{id}', 'getClient');
+$app->get('/getclient', 'getClient');
 $app->post('/client', 'addClient');
 $app->put('/client/{id}', 'updateClient');
 $app->delete('/client/{id}', 'deleteClient');
+$app->get('/connexion', '');
+
+function login ($request, $response, $args) {
+    $login = "swann";
+    $pwd = "swann";
+    
+    $body = $request->getParsedBody(); // Parse le body
+    $enteredLogin = $body['login'];
+    $enteredPwd = $body['motDePasse']; 
+
+    if($enteredLogin==$login && $enteredPwd==$pwd) {
+        $issuedAt = time();
+        $expirationTime = $issuedAt + 60; // jwt valid for 60 seconds from the issued time
+        $payload = array(
+        'login' => $enteredLogin,
+        'iat' => $issuedAt,
+        'exp' => $expirationTime
+        );
+        $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
+        
+        $response = $response->withHeader("Authorization", "Bearer {$token_jwt}")->withHeader("Content-
+        Type", "application/json");
+        
+        $data = array('login' => $login, 'pwd' => $pwd, 'token' => $token_jwt);
+        return $response->withJson($data);
+    }
+    $data = array("erreur" => 'Erreur!', 'token' => "Erreur de login");
+    return $response->withJson($data);
+}
+$app->post('/login', login);
 
 function setHeader($response) {
     return $response->withHeader("Access-Control-Allow-Origin", "*")->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -20,13 +76,12 @@ function getProduits($resquest,$response,$args)
 {
     $url = 'data.json'; // path to your JSON file
     $data = file_get_contents($url);
-    return setHeader($response)->write($data);              
+    return $response->write($data);              
 }
 
 function addClient($request,$response,$args) {
-    
     $body = $request->getParsedBody(); // Parse le body
-    
+    $client = array();
     $client->nom = $body['nom']; // Data du formulaire
     $client->prenom = $body['prenom'];
     $client->civilite = $body['civilite'];
@@ -38,6 +93,8 @@ function addClient($request,$response,$args) {
     $client->email = $body['email'];
     $client->login = $body['login'];
     $client->password = $body['password'];
+    
+    return $response->write(json_encode($client));
 }
 
 function getClient($request,$response,$args) {
--- back/vendor/composer/autoload_static.php
+++ back/vendor/composer/autoload_static.php
@@ -13,10 +13,12 @@ class ComposerStaticInit7ad6103dbed26bde69ecc169652475b6
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Slim\\Middleware\\' => 16,
             'Slim\\' => 5,
         ),
         'P' => 
         array (
+            'Psr\\Log\\' => 8,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
         ),
@@ -26,14 +28,23 @@ class ComposerStaticInit7ad6103dbed26bde69ecc169652475b6
         ),
         'F' => 
         array (
+            'Firebase\\JWT\\' => 13,
             'FastRoute\\' => 10,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Slim\\Middleware\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tuupola/slim-jwt-auth/src',
+        ),
         'Slim\\' => 
         array (
             0 => __DIR__ . '/..' . '/slim/slim/Slim',
+        ),
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
         'Psr\\Http\\Message\\' => 
         array (
@@ -46,6 +57,10 @@ class ComposerStaticInit7ad6103dbed26bde69ecc169652475b6
         'Interop\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
+        ),
+        'Firebase\\JWT\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
         ),
         'FastRoute\\' => 
         array (
